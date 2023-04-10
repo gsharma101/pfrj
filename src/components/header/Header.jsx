7b658dc5a1112e6fa723da26d7afd4773ddb51f5
unknown <sharmag8121@gmail.com>
2023-04-10 12:55:55 +0530

    partially completed header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,11 +6,12 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header__left">
-        <h1 className="header__heading">Hi,I'am Gaurav Sharma</h1>
+        <h1 className="header__heading">
+          Hi, 👋 I'am <span>Gaurav Sharma</span>
+        </h1>
       </div>
 
-      <div className="header__rigth">
-        <h1> Right </h1>
+      <div className="header__right">
         <img className="headerImg" src={logo} alt="headerimg" />
       </div>
     </div>
