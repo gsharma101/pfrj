89d148e1a18fab18b5d0f146069614c2ba5f78a1
unknown <sharmag8121@gmail.com>
2023-04-11 04:22:01 +0530

    header completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,9 +6,17 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header__left">
-        <h1 className="header__heading">
-          Hi, 👋 I'am <span>Gaurav Sharma</span>
-        </h1>
+        <div className="header__left--box">
+          <h1 className="header__heading">
+            Hi, 👋 I'am <span>Gaurav Sharma</span>
+          </h1>
+          <a
+            className="resume"
+            href="https://drive.google.com/file/d/1nmbemU-clB9-QE9a4ufEa_5jLk7vzCOn/view"
+          >
+            Download CV
+          </a>
+        </div>
       </div>
 
       <div className="header__right">
