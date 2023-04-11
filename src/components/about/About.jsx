911ce7cdb48c0a6e7fb33d710bb3d2ba93780222
unknown <sharmag8121@gmail.com>
2023-04-12 03:50:41 +0530

    started with about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./about.scss";
+import aboutImg from "../../assets/images/about.jpg";
 
 const About = () => {
   return (
     <div className="about">
-      <div className="about__left">
-        <h1 className="section_heading">a little about me</h1>
+      <h1 className="section_heading">a little about me</h1>
+      <div className="section_box">
+        <div className="about__left">
+          <img src={aboutImg} alt="aboutimg" />
+        </div>
+        <div className="about__rigth">
+          <h3>I'am Gaurav Sharma</h3>
+          <p>
+            junior software developer from India. I'm deeply passionate about
+            software development, computers and cyber security. Apart from that
+            I have a habit of reading books of different genres like self-help,
+            business & non-fiction etc;
+          </p>
+        </div>
       </div>
-      <div className="about__rigth"></div>
     </div>
   );
 };
