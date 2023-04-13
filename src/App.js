098a0d6617d5487ffe41375bd79fc1a195eac5d8
unknown <sharmag8121@gmail.com>
2023-04-14 00:49:06 +0530

    skills section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Header, About } from './components';
+import { Navbar, Header, About, Skills } from './components';
 
 import './App.scss'
 
@@ -10,6 +10,7 @@ const App = () => {
             <Navbar />
             <Header />
             <About />
+            <Skills />
         </div>
     )
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./skills.scss";
 
 function Skills() {
-  return <div className="skills"></div>;
+  return (
+    <div className="skills" id="skills">
+      <div className="section_heading"> My Skills</div>
+      <div className="section_box">
+        <div className="skills__left"></div>
+        <div className="skills__right"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
