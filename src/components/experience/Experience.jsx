29751099b95f751b496969bce501b55d3f8581bf
unknown <sharmag8121@gmail.com>
2023-04-21 02:15:56 +0530

    component skills completed

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,6 +4,32 @@ function Experience() {
   return (
     <section className="experience" id="experience">
       <div className="section_heading">Experience</div>
+      <div className="timeline">
+        <div className="outer">
+          <div className="card">
+            <div className="info">
+              <h3 className="title">Title 1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="info">
+              <h3 className="title">Title 2</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,7 +7,39 @@ function Skills() {
     <section className="skills" id="skills">
       <div className="section_heading">Skills</div>
       <div className="section__container">
-        <div className="section__container--left left"></div>
+        <div className="section__container--left left">
+          <div className="skills__box">
+            <div className="skill__data u-margin-bottom-medium">
+              <div class="skill__name">
+                <span>Full-Stack Development</span>
+              </div>
+              <div class="skill__bar skill__1"></div>
+              <div class="skill__percentage">
+                <span>85%</span>
+              </div>
+            </div>
+
+            <div className="skill__data u-margin-bottom-medium">
+              <div class="skill__name">
+                <span>Data Structures and Algorithms</span>
+              </div>
+              <div class="skill__bar skill__2"></div>
+              <div class="skill__percentage">
+                <span>75%</span>
+              </div>
+            </div>
+
+            <div className="skill__data">
+              <div class="skill__name">
+                <span>Cyber Security</span>
+              </div>
+              <div class="skill__bar skill__3"></div>
+              <div class="skill__percentage">
+                <span>65%</span>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="section__container--right right">
           <img src={techLogo} alt="techlogo" />
         </div>
