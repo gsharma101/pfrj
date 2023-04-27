a67134b929a007df6b49f98a6593ba35447e9153
unknown <sharmag8121@gmail.com>
2023-04-27 23:21:59 +0530

    learning description

--- src/components/learning/Learning.jsx
+++ src/components/learning/Learning.jsx
@@ -4,6 +4,12 @@ function Learning() {
   return (
     <section className="learning" id="learning">
       <div className="section_heading">Learning</div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea est libero
+        autem perspiciatis doloribus soluta consectetur asperiores impedit quia,
+        at consequatur? Accusamus eligendi sint distinctio quidem, sequi
+        laudantium. Sint, corporis!
+      </p>
     </section>
   );
 }
